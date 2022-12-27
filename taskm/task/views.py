20a2e0a445e84281d539d82f96da4516fc0506a5
twilight2017 <liuzixuanp@163.com>
2022-12-27 10:53:31 +0800

    feat: add basic operation

---
+++ taskm/task/views.py
@@ -0,0 +1,25 @@
+from django.urls import reverse, reverse_lazy
+from .models import Task
+from .forms import TaskForm
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+# Create your views here.
+
+
+class TaskListView(ListView):
+    model = Task
+    context_object_name = 'tasks'
+
+
+class TaskDetailView(DetailView):
+    model = Task
+
+
+class TaskCreateView(CreateView):
+    model = Task
+    form_class = TaskForm
+    success_url = reverse_lazy("tasks:task_list")
+
+
+class TaskDeleteView(DeleteView):
+    model = Task
+    success_url = reverse_lazy("tasks:task_list")
